e078ae6c244d47ed1da643dce6ba45277098fc5f
Anandre <Anandrep17@gmail.com>
2022-04-20 02:05:46 +0200

    Added distinct items function
    Added distinct items function to use for identifier in iteration

--- src/src.py
+++ src/src.py
@@ -1,10 +1,22 @@
 src_data = []
+distinct_items = []
 
-with open('doc folder/source_data.csv', 'r') as f:
-    next(f)
-    for line in f:
+
+def read_src_data():
+    with open('doc folder/source_data.csv', 'r') as f:
+        next(f)
+        for line in f:
             words = line.split(',')
             src_data.append((words[0], words[1], words[2].replace("\n","")))
-    src_data.sort()
+        src_data.sort()
+    return src_data
 
-print(src_data)
+print(read_src_data())
+
+def get_distinct_items():
+    src_data_count = len(src_data)
+    for i in range(src_data_count):
+        if src_data[i][0] != src_data[i-1][0]:
+            distinct_items.append(src_data[i][0])
+    return(distinct_items)
+print(get_distinct_items())
